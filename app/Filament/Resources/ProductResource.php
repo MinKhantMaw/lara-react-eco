cb1f15b413369c38c50fcbfb607902668ca644b4
mkhant <minkhantmaw@onenex.co>
2025-03-18 21:08:30 +0630

    feat/product image show and setup media library

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources;
 use App\Enums\ProductStatusEnum;
 use App\Enums\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\Pages\EditProduct;
+use App\Filament\Resources\ProductResource\Pages\ProductImage;
 use App\Models\Product;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\Grid;
@@ -12,8 +14,11 @@ use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Pages\SubNavigationPosition;
+use Filament\Resources\Pages\Page;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
@@ -24,7 +29,9 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-s-queue-list';
+
+    protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
 
     public static function form(Form $form): Form
     {
@@ -94,6 +101,7 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
+                SpatieMediaLibraryImageColumn::make('images')->collection('images')->limit(1)->label('Image')->conversion('thumb'),
                 TextColumn::make('title')->sortable()->words(10)->searchable(),
                 TextColumn::make('status')->badge()->colors(ProductStatusEnum::colors()),
                 TextColumn::make('department.name')->sortable()->searchable(),
@@ -130,7 +138,18 @@ class ProductResource extends Resource
             'index' => Pages\ListProducts::route('/'),
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
+            'images' => Pages\ProductImage::route('/{record}/images'),
         ];
+    }
+
+    public static function getRecordSubNavigation(Page $page): array
+    {
+        return
+            $page->generateNavigationItems([
+                EditProduct::class,
+                ProductImage::class,
+            ]);
+
     }
 
     public static function canViewAny(): bool
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
+    public function registerAllMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(100);
+        $this->addMediaConversion('small')->width(480);
+        $this->addMediaConversion('large')->width(1200);
+    }
+
     /**
      * Get the user that owns the Product
      *
